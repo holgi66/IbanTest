15dd6a59d30d1901b71b053f8713357f50dd731c
holgi66 <holger-strohmeyer_stdent2021@wilder.school>
2022-03-07 11:33:16 +0100

    20220307 ibantest

---
+++ src/test/java/IbanTest.java
@@ -0,0 +1,34 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import de.telekom.sea7.Iban;
+
+public class IbanTest {
+
+	private final Iban cut;
+
+	@BeforeEach
+	void setup() {
+		cut = new Iban();
+	}
+
+	@Test
+	void setIban_test() {
+		// Arrange
+		cut.setIban("DE213456789");
+
+		// Act
+		var result = cut.getIban();
+
+		// Assert
+		assertEquals("DE213456789", result);
+	}
+
+	@AfterEach
+	void teardown() {
+		cut = null;
+	}
+}
