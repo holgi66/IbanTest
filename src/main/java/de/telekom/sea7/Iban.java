7314dd0893fc60cddd81543d05e794840b4ebd7a
holgi66 <holger-strohmeyer_stdent2021@wilder.school>
2022-03-07 11:04:25 +0100

    20220307 iban klasse erzeugt

---
+++ src/main/java/de/telekom/sea7/Iban.java
@@ -0,0 +1,24 @@
+package de.telekom.sea7;
+
+public class Iban {
+
+	private int id;
+	private String iban;
+
+	public int getId() {
+		return id;
+	}
+	
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getIban() {
+		return iban;
+	}
+
+	public void setIban(String iban) {
+		this.iban = iban;
+	}
+
+}
