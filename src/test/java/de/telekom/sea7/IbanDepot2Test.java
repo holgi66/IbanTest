31c425a4ffc112dee72fde97084c3730a27aaab4
holgi66 <holger-strohmeyer_stdent2021@wilder.school>
2022-03-08 08:02:01 +0100

    20220308 Start

--- src/test/java/de/telekom/sea7/IbanDepot2Test.java
+++ src/test/java/de/telekom/sea7/IbanDepot2Test.java
@@ -3,7 +3,6 @@ package de.telekom.sea7;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.NullSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 public class IbanDepot2Test {
